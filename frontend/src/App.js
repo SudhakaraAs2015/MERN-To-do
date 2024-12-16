2dc20a7ce6361a20367d8881de669e6848ca7f31
Sudhakara H N <sudhakarahn29@gmail.com>
2024-12-16 16:15:06 +0530

    Created login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
     <div className="App">
+      <LoginPage/>
     </div>
   );
 }
--- frontend/src/components/api.jsx
+++ frontend/src/components/api.jsx
@@ -2,3 +2,7 @@ import  axios  from 'axios';
 
 const API_URI = 'http://localhost:5000/api/todos';
 
+const getTodo =()=>axios.get(API_URI);
+const createTodo =(todo)=>axios.post(API_URI, todo);
+const updateTodo=(id,todo)=>axios.put(`${API_URI}/${id}`,todo);
+const deleteTodo =(id)=>axios.delete(`${API_URI}/${id}`);
